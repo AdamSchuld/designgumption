79b5dcd442fad4cb7122ea0265df321d2fc25389
Adam Schuld <designgumption@gmail.com>
2014-02-09 17:59:19 -0600

    Further modularizes css, beginning of Susy functionality, restructuring of index.html

--- config.rb
+++ config.rb
@@ -1,5 +1,14 @@
 require "susy"
+http_path = "/"
+
 css_dir = '_/css'
+img_dir = '_/images'
 sass_dir = '_/components/sass'
+fonts_dir = '_/css/fonts'
 javascripts_dir = '_/js'
+
+# output_style = :expanded, :nested, :compact, :compressed
 output_style = :nested
+
+# To enable relative paths to assets via compass helper functions
+relative_assets = true
