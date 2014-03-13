03aaba186b69f09bd4a387f21b3e1d3bbb3cb6e8
Adam Schuld <designgumption@gmail.com>
2014-03-12 19:00:46 -0700

    Attempt at IE fix for text rotator"

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ fonts_dir = '_/css/fonts'
 javascripts_dir = '_/js'
 
 # output_style = :expanded, :nested, :compact, :compressed
-output_style = :compressed
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions
 relative_assets = true
